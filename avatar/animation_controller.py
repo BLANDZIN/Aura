64f405c315e7c79d446752b46c9b25ad52c960b9
BLAND <126494585+BLANDZIN@users.noreply.github.com>
2026-07-25 15:04:49 -0300

    Revert "feat: integrate configurable VRM avatar engine"

--- avatar/animation_controller.py
+++
@@ -1,3 +0,0 @@
-class AnimationController:
-    def __init__(self, config): self.config = config
-    def animation_for(self, state): return self.config.get('states', {}).get(state, {}).get('animation', state)
--- avatar/expression_controller.py
+++
@@ -1,4 +0,0 @@
-class ExpressionController:
-    def __init__(self, config): self.expressions = config.get('expressions', {})
-    def available(self): return tuple(self.expressions)
-    def select(self, name): return name if name in self.expressions else 'neutral'
